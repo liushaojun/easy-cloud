302d8d17b1e13ca5751a7dc171bec0098eca4daa
liushaojun <l785029095>
2018-06-15 18:51:09 +0800

    :fire: Removing turbine module.

--- ec-dashboard/src/main/java/com/brook/dashboard/EcDashboardApplication.java
+++ ec-dashboard/src/main/java/com/brook/dashboard/EcDashboardApplication.java
@@ -3,9 +3,14 @@ package com.brook.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * @author brook
+ */
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class EcDashboardApplication {
 
   public static void main(String[] args) {
